e0188d2b349f35c90e698040fb59ea69a80f9b8e
ging-dev <gingdz11012001@gmail.com>
2020-05-27 13:54:22 +0700

    Add phpunit test

---
+++ tests/ChatfuelTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gingdev\Chatfuel;
+
+use Gingdev\Chatfuel;
+use PHPUnit\Framework\TestCase;
+
+class ChatfuelTest extends TestCase
+{
+    public function sendText() {
+        $chatfuel = new Chatfuel();
+        $chatfuel->sendText('Test');
+        $test = [
+            'messages' => [
+                'text' => 'Test'
+            ]
+        ];
+        $this->assertSame($chatfuel->toArray(), $test);
+    }
+}
