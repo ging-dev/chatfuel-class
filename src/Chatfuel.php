c38ba6da40bba178c0b4eee5c1771aedcb628ea4
ging-dev <gingdz11012001@gmail.com>
2020-05-27 13:10:49 +0700

    Add some method

--- src/Chatfuel.php
+++ src/Chatfuel.php
@@ -8,13 +8,15 @@ use InvalidArgumentException;
 
 class Chatfuel
 {
+    const INVALID_URL = 'Error: Invalid URL!';
+
     /** @var array $response */
     protected $response = [];
 
     /**
-     * Add text-message to response
+     * Send Text
      *
-     * @param string|array $message
+     * @param string|array $messages
      * @return $this
      */
     public function sendText($messages)
@@ -24,22 +26,61 @@ class Chatfuel
         }
         switch (gettype($messages)) {
             case 'string':
-                $this->response[] = [
-                    'text' => $messages
-                ];
+                $this->response[] = ['text' => $messages];
                 break;
             case 'array':
                 foreach ($messages as $message) {
-                    $this->response[] = [
-                        'text' => $message
-                    ];
+                    $this->response[] = ['text' => $message];
                 }
                 break;
             default:
-                $this->response[] = [
-                    'text' => 'Error!'
-                ];
+                $this->response[] = ['text' => 'Error!'];
                 break;
         }
+        return $this;
+    }
+    
+    /**
+     * Send Image
+     * 
+     * @param string $url
+     * @return $this
+     */
+    public function sendImage(string $url)
+    {
+        if (isURL($url)) {
+            $this->sendAttachment('image', ['url' => $url]);
+        } else {
+            $this->sendText(self::INVALID_URL);
+        }
+        return $this;
+    }
+    
+    /**
+     * Send Attachment
+     * 
+     * @param string $type
+     * @param array $payload
+     * return $this
+     */
+    protected function sendAttachment(string $type, array $payload)
+    {
+        $this->response[] = [
+            'attachment' => [
+                'type'    => $type,
+                'payload' => $payload
+            ]
+        ];
+        return $this;
+    }
+    /**
+     * Check the valid URL
+     * 
+     * @param string $url
+     * @return boolean
+     */
+    protected function isURL(string $url): bool
+    {
+        return filter_var($url, FILTER_VALIDATE_URL);
     }
 }
