80c4cf943ab309835cdd17bea97c58b5e6092820
ging-dev <gingdz11012001@gmail.com>
2020-05-27 10:27:30 +0700

    Add sendText() method

---
+++ Readme.md
@@ -0,0 +1,6 @@
+## Chatfuel class for PHP
+
+[![StyleCI](https://github.styleci.io/repos/267200397/shield?branch=master)](https://github.styleci.io/repos/267200397)
+
+## Install
+    composer require ging-dev/chatfuel
--- src/Chatfuel.php
+++ src/Chatfuel.php
@@ -4,6 +4,42 @@ declare(strict_types=1);
 
 namespace Gingdev;
 
+use InvalidArgumentException;
+
 class Chatfuel
 {
+    /** @var array $response */
+    protected $response = [];
+    
+    /**
+     * Add text-message to response
+     *
+     * @param string|array $message
+     * @return $this
+     */
+    public function sendText($messages)
+    {
+        if (empty($messages)) {
+            throw new InvalidArgumentException('Input must not empty.');
+        }
+        switch (gettype($messages)) {
+            case 'string':
+                $this->response[] = [
+                    'text' => $messages
+                ];
+                break;
+            case 'array':
+                foreach ($messages as $message) {
+                    $this->response[] = [
+                        'text' => $message
+                    ];
+                }
+                break;
+            default:
+                $this->response[] = [
+                    'text' => 'Error!'
+                ];
+                break;
+        }
+    }
 }
