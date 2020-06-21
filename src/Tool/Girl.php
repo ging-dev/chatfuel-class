4faeb3e8f4826ba011c59664f4b34048c75438dd
ging-dev <gingdz11012001@gmail.com>
2020-06-21 05:13:08 +0000

    Apply fixes from StyleCI

--- src/Tool/Girl.php
+++ src/Tool/Girl.php
@@ -8,30 +8,30 @@ use Curl\Curl;
 
 class Girl
 {
-	/**
-	 * @var Curl
-	 */
-	private $curl;
+    /**
+     * @var Curl
+     */
+    private $curl;
 
-	/**
-	 * @var int
-	 */
-	private $total;
+    /**
+     * @var int
+     */
+    private $total;
 
-	public function __construct()
-	{
-		$this->curl = new Curl();
-		$this->curl->get('https://api.bumgada.com/api/v1/medias?limit=1');
-		$this->total = json_decode($this->curl->response, true)['data']['total'];
-	}
+    public function __construct()
+    {
+        $this->curl = new Curl();
+        $this->curl->get('https://api.bumgada.com/api/v1/medias?limit=1');
+        $this->total = json_decode($this->curl->response, true)['data']['total'];
+    }
 
-	/**
-	 * Get random one record
-	 * @return string
-	 */
-	public function getOne(): string
-	{
-		$this->curl->get('https://api.bumgada.com/api/v1/medias?limit=1&page=' . rand(1, $this->total));
-		return json_decode($this->curl->response, true)['data']['medias'][0]['h'];
-	}
+    /**
+     * Get random one record
+     * @return string
+     */
+    public function getOne(): string
+    {
+        $this->curl->get('https://api.bumgada.com/api/v1/medias?limit=1&page=' . rand(1, $this->total));
+        return json_decode($this->curl->response, true)['data']['medias'][0]['h'];
+    }
 }
